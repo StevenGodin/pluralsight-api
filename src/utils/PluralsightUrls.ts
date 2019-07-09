93dbb4b98b27d0eeccd7db3b47a51b81ee1747da
Steven Godin <contact@stevengodin.ca>
2019-07-08 20:48:09 -0400

    add tslint and prettier config

--- src/utils/PluralsightUrls.ts
+++ src/utils/PluralsightUrls.ts
@@ -1,7 +1,9 @@
 import PluralsightOptions from "../PluralsightOptions";
 
 export const createQueryString = (query?: { [key: string]: string | undefined }) => {
-  if (!query) return "";
+  if (!query) {
+    return "";
+  }
 
   return Object.keys(query)
     .filter(key => query[key] !== undefined)
@@ -12,31 +14,29 @@ export const createQueryString = (query?: { [key: string]: string | undefined })
 export default class PluralsightUrls {
   constructor(private options: PluralsightOptions) {}
 
-  getReportsUrl(endpointName: string, filter?: { [key: string]: string | undefined }) {
+  public getReportsUrl(endpointName: string, filter?: { [key: string]: string | undefined }) {
     const { planId, apiToken, reportsApiUrl } = this.options;
-    if (!reportsApiUrl) throw "A reportsApiUrl is required";
-    if (!planId || !apiToken) throw "A planId and apiToken are required";
+    if (!reportsApiUrl) { throw new Error("A reportsApiUrl is required"); }
+    if (!planId || !apiToken) { throw new Error("A planId and apiToken are required"); }
     const queryString = createQueryString(filter);
-    return `${reportsApiUrl}/${endpointName}/${planId}?token=${apiToken}${
-      queryString ? `&${queryString}` : ""
-    }`;
+    return `${reportsApiUrl}/${endpointName}/${planId}?token=${apiToken}${queryString ? `&${queryString}` : ""}`;
   }
 
-  getCourseUsageUrl(query?: { [key: string]: string | undefined }) {
+  public getCourseUsageUrl(query?: { [key: string]: string | undefined }) {
     return this.getReportsUrl("course-usage", query);
   }
 
-  getCourseCompletionUrl(query?: { [key: string]: string | undefined }) {
+  public getCourseCompletionUrl(query?: { [key: string]: string | undefined }) {
     return this.getReportsUrl("course-completion", query);
   }
 
-  getUsersUrl() {
+  public getUsersUrl() {
     return this.getReportsUrl("users");
   }
 
-  getCourseCatalogUrl() {
+  public getCourseCatalogUrl() {
     const { coursesApiUrl } = this.options;
-    if (!coursesApiUrl) throw "A coursesApiUrl is required";
+    if (!coursesApiUrl) { throw new Error("A coursesApiUrl is required"); }
     return `${coursesApiUrl}/courses`;
   }
 }
